0cad68f5e521316ff1d6dfc1ad64936350174107
Gelio <voreny.gelio@gmail.com>
2016-02-05 11:14:55 +0100

    undone data slicing, made it shorter

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,10 +26,9 @@
             values = data.data.map(function(currData) { return currData[1]});*/
 
         var chartData = data.data;
-        chartData = chartData.slice(0, 20);
 
         var width = 420,
-            barHeight = 20;
+            barHeight = 2;
 
         // Render data on the page
         var chart = d3.select('.chart'),
@@ -52,16 +51,11 @@
             .attr('width', function(d) { return x(d[1]) + '%'})
             .attr('height', barHeight-1);
 
-        bar.append('text')
+        /*bar.append('text')
             .attr('x', function(d) { return (x(d[1]) - 0.5) + '%'; })
             .attr('y', barHeight/2)
             .attr('dy', '.35em')
-            .text(function(d) { return d[0] + ' - ' + d[1]});
-
-        /*chart.selectAll('div').data(values)
-            .enter().append('div')
-            .style('width', function(d) {return x(d) + 'px';})
-            .text(function(d) { return d;});*/
+            .text(function(d) { return d[0] + ' - ' + d[1]});*/
     }
 
     function highlightBar() {
