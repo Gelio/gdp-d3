ceaa1fb36812311bf644aff936e9b9e0bac680ff
Gelio <voreny.gelio@gmail.com>
2016-02-05 10:45:00 +0100

    added mouseover highlighting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,9 +19,57 @@
     }
 
     function downloadCompleted(data) {
-        console.log('Got data');
-        console.log(data.data);
+        $('.chart').html('Data was fetched from the server. It\'s now processed and will be displayed shortly.');
+
+        // Refactor that into one loop for more efficiency
+        /*var periods = data.data.map(function(currData) { return currData[0]}),
+            values = data.data.map(function(currData) { return currData[1]});*/
+
+        var chartData = data.data;
+
+        var width = 420,
+            barHeight = 20;
 
         // Render data on the page
+        var chart = d3.select('.chart'),
+            x = d3.scale.linear()
+                .domain([0, d3.max(chartData, function(d) { return d[1];})])
+                .range([0, 100]);
+
+        chart.attr('width', '100%')
+            .attr('height', barHeight * chartData.length + 'px');
+
+        var bar = chart.selectAll('g')
+            .data(chartData)
+            .enter().append('g')
+            .attr('transform', function(d, i) { return 'translate(0, ' + i * barHeight + ')'; })
+            .on('mouseover', highlightBar)
+            .on('mouseout', removeHighlightAndTooltip);
+            //.on('mousemove', displayTooltip);
+
+        bar.append('rect')
+            .attr('width', function(d) { return x(d[1]) + '%'})
+            .attr('height', barHeight-1);
+
+        bar.append('text')
+            .attr('x', function(d) { return (x(d[1]) - 0.5) + '%'; })
+            .attr('y', barHeight/2)
+            .attr('dy', '.35em')
+            .text(function(d) { return d[0] + ' - ' + d[1]});
+
+        /*chart.selectAll('div').data(values)
+            .enter().append('div')
+            .style('width', function(d) {return x(d) + 'px';})
+            .text(function(d) { return d;});*/
+    }
+
+    function highlightBar() {
+        d3.select(this)
+            .classed('highlighted', true)
+    }
+
+    function removeHighlightAndTooltip() {
+        d3.select(this)
+            .classed('highlighted', false)
     }
 })();
